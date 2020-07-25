e6b4d0728f771ca594eaf27a3d553c5cb68b8ca4
laftaria1 <68764475+laftaria1@users.noreply.github.com>
2020-07-25 12:23:36 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -1,8 +1,3 @@
-
-# 봉순#4888 : MASS DM BOT SOURCE
-# 오픈소스 이용하여 제작되었습니다
-
-
 import discord
 import asyncio
 import datetime
@@ -12,26 +7,26 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('라프라프')
     await client.change_presence(status=discord.Status.online, activity=game)
 
-#/dm {할말}로 전체DM 전송
+#!dm {할말}로 전체DM 전송
 @client.event
 async def on_message(message):
-    if message.content.startswith('/dm'):
+    if message.content.startswith('!dm'):
         for i in message.guild.members:
             if i.bot == True:
                 pass
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == ★자신의 디스코드ID를 적어주세요(퍼미션):
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="★★최상단 제목★★")
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text=f"discord.gg/★★서버초대코드★★")
+                    if message.author.id ==720927829193392189:
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="DM보내기")
+                        embed.add_field(name="라프라프!", value=msg, inline=True)
+                        embed.set_footer(text=f"discord.gg/9hBP7fP")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzM2NDEzMzYyOTI5NTMyOTMw.XxucYw.687eaW6-s8iO5wwTXwNFdV2aJvw')
